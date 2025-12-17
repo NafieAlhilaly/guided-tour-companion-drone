6f310e3f86b6f77a24c64f7ff97b19b535d8bfa8
nafea <warrior259@gmail.com>
2025-12-17 18:48:24 +0300

    chore(scripts): add med alert pub example script

--- scripts/pub.sh
+++ scripts/pub.sh
@@ -1,3 +1,6 @@
 # Publish new group location
 new_location="18.375843,42.3782481, 0"
-mosquitto_pub -t "flollow/target_location" -m "$new_location"
+mosquitto_pub -t "flollow/target_location" -m "18.373417, 42.3781843,10"
+
+# Publish alert for medical supply
+mosquitto_pub -t "supplies/medical" -m "med_alert"
