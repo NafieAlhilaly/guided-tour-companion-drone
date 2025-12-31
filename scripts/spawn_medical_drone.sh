f5403c351522eaa158e55a10e14e496adc0ad91d
nafea <warrior259@gmail.com>
2026-01-01 01:25:03 +0300

    chore(scritps): add a script to spawn new drone instance

---
+++ scripts/spawn_medical_drone.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+# Check if is PXfirst instance is running, if its not, exit the script
+PX4_RUNNING=$(pgrep -f px4_sitl_default)
+if [ -z "$PX4_RUNNING" ]; then
+    echo "PX4 instance is not running. Please start PX4 first."
+    exit 1
+fi
+
+
+i=1
+cd ./PX4-Autopilot
+gnome-terminal -- bash -c "PX4_UXRCE_DDS_NS=px4_1 PX4_SYS_AUTOSTART='400${i}' PX4_SIM_MODEL=gz_x500_depth PX4_GZ_MODEL_POSE='15,27,0' ./build/px4_sitl_default/bin/px4 -i $i; exec bash"
+
